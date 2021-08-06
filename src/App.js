a2b1a33569bfb5e0ce46a245ea5e4ef67f28201d
Deborah Botchwey <deborahbotchwey@gmail.com>
2021-08-06 16:55:54 -0600

    Search engine added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.png';
+import Dictionary from './Dictionary'
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
+          <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
-    
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="App-footer"><small>Coded by Deb</small></footer>
+      </div>
     </div>
   );
 }
