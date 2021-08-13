58dedca912665766c9784c9879e9d2af02c1dc85
Deborah Botchwey <deborahbotchwey@gmail.com>
2021-08-12 18:32:27 -0600

    Added phonetics audio link and text

--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props){
 
     if(props.results){
-        console.log(props.results);
         return(
         <div className="Results">
             <h2>{props.results.word}</h2>
+            {props.results.phonetics.map(function(phonetic,index){
+                return (
+                    <div key={index}>
+                        <Phonetics phonetics={phonetic}/>
+                    </div>
+                    );
+            })}
+            
            { props.results.meanings.map(function(meaning, index){
-
-               return (<div key ={index}><Meaning meaning={meaning}/></div>);
+               return (<div key={index}><Meaning meaning={meaning}/></div>);
            })}
         </div>);
     }else{
